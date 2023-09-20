93f2ccf8b03728ae0af70b2bc85042a276caf6f1
JonasElburgUVA <114097649+JonasElburgUVA@users.noreply.github.com>
2023-09-20 14:46:28 +0200

    notebook runs on m1 + added dataloader class

---
+++ dataloader.py
@@ -0,0 +1,29 @@
+import h5py
+import torch
+from torch.utils.data import Dataset
+from typing import Tuple
+
+class PatchCamelyonDataset(Dataset):
+    def __init__(self, data_path: str, targets_path: str, transform=None) -> None:
+        self.data = h5py.File(data_path)["x"]
+        self.targets = h5py.File(targets_path)["y"]
+        self.transform = transform
+
+    def __len__(self) -> int:
+        return self.targets.shape[0]
+
+    def __getitem__(self, idx: int) -> Tuple[torch.tensor, torch.tensor]:
+        sample = torch.tensor(self.data[idx, :, :, :]).float() / 255.0
+        # [channels, x, y] to [x, y, channels]
+        sample = torch.permute(sample, (2, 0, 1))
+
+        # We need to squeeze the targets as they are
+        # nested within multiple arrays
+        target = torch.tensor(self.targets[idx].squeeze())
+
+        if self.transform:
+            sample = self.transform(sample)
+        
+        return sample, target
+    
+
