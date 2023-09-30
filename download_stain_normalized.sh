679822bac748c1e3990fc73661d1641583c28445
Walter Simoncini <walter@ashita.nl>
2023-09-30 18:26:08 +0200

    added a script to download stain-normalized data and job files for stain-normalized models

---
+++ download_stain_normalized.sh
@@ -0,0 +1,15 @@
+# NOTE: The bucket must allow public access before data can be downloaded
+# Create a directory for the data
+mkdir -p data/stain
+
+# Training data
+wget https://storage.googleapis.com/download/storage/v1/b/pcam-stain/o/camelyonpatch_level_2_split_train_x.h5 -P data/stain
+wget https://storage.googleapis.com/download/storage/v1/b/pcam-stain/o/camelyonpatch_level_2_split_train_y.h5 -P data/stain
+
+# Validation data
+wget https://storage.googleapis.com/download/storage/v1/b/pcam-stain/o/camelyonpatch_level_2_split_valid_x.h5 -P data/stain
+wget https://storage.googleapis.com/download/storage/v1/b/pcam-stain/o/camelyonpatch_level_2_split_valid_y.h5 -P data/stain
+
+# Test data
+wget https://storage.googleapis.com/download/storage/v1/b/pcam-stain/o/camelyonpatch_level_2_split_test_x.h5 -P data/stain
+wget https://storage.googleapis.com/download/storage/v1/b/pcam-stain/o/camelyonpatch_level_2_split_test_y.h5 -P data/stain
